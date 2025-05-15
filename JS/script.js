1a623ab92c9793489e62fdfe06e348fbe99e69d4
hamzalachguer <lachguerhmza@gmail.com>
2025-05-16 00:10:41 +0200

    fixing announcement bar

--- JS/script.js
+++ JS/script.js
@@ -10,14 +10,16 @@
 /* 
   ANOUNCEMENT BAR
 */
-document.addEventListener("DOMContentLoaded", () => {
-  const announcementBar = document.querySelector(".announcement__bar");
-  const announcementContent = Array.from(announcementBar.children);
+window.addEventListener('DOMContentLoaded', () => {
+  const scroller = document.querySelector(".scroller");
+  const bar = document.querySelector(".announcement__bar");
+  const barItems = Array.from(bar.children);
 
-  announcementContent.forEach(i => {
-    const duplicatedItem = i.cloneNode(true);
-    duplicatedItem.setAttribute("aria-hidden", true);
-    announcementBar.appendChild(duplicatedItem)
+  // position the duplicate content
+  barItems.forEach(i => {
+    const duplicate = i.cloneNode(true);
+    duplicate.setAttribute("aria-hidden", true);
+    bar.appendChild(duplicate);
   })
 })
 
@@ -27,7 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
 */
 const menuBtn = document.querySelector(".menu__icon");
 const navList = document.querySelector(".nav__list");
-const header  = document.querySelector(".header");
 // const body = document.body;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -35,12 +36,6 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu () {
   const isOpen = menuBtn.classList.toggle("open");
   navList.classList.toggle("nav__hidden");
-  header.classList.toggle("active-state");
-  document.querySelector(".logo img").src = "https://framerusercontent.com/images/2hmyf0F7i4RXrF0rxEc4Wv7IHA.svg";
-
-  if (!isOpen) {
-    document.querySelector(".logo img").src = "https://framerusercontent.com/images/1g6P50scDphvftPgEAObATb50HI.svg";
-  }
 
   menuBtn.setAttribute("aria-expanded", isOpen);
   navList.setAttribute("aria-hidden", !isOpen);
