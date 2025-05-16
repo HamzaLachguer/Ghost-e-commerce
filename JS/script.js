b64b9fb555f67227a4b917e8960504c4c28cee57
hamzalachguer <lachguerhmza@gmail.com>
2025-05-17 01:06:49 +0200

    finishing the header

--- JS/script.js
+++ JS/script.js
@@ -30,12 +30,25 @@ window.addEventListener('DOMContentLoaded', () => {
 const menuBtn = document.querySelector(".menu__icon");
 const navList = document.querySelector(".nav__list");
 // const body = document.body;
+const windowWidth = window.innerWidth;
+const header = document.querySelector(".header");
+
+const logo = document.querySelector(".logo img")
+
+/* logo versions */
+const logoBlack = "https://framerusercontent.com/images/2hmyf0F7i4RXrF0rxEc4Wv7IHA.svg";
+const logoWhite = "https://framerusercontent.com/images/1g6P50scDphvftPgEAObATb50HI.svg";
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu () {
   const isOpen = menuBtn.classList.toggle("open");
   navList.classList.toggle("nav__hidden");
+  
+  if (isOpen && windowWidth < 810) {
+    header.classList.toggle("active-state");
+    logo.src = logoBlack;
+  } 
 
   menuBtn.setAttribute("aria-expanded", isOpen);
   navList.setAttribute("aria-hidden", !isOpen);
@@ -52,3 +65,25 @@ navList.id = "nav-list";
 /* 
   REMOVING THE"ACTIVE-STATE" FROM THE HEADER
 */
+const heroHeight = document.querySelector(".hero").offsetHeight;
+
+window.addEventListener('scroll', () => {
+  if (!menuBtn.classList.contains("open")) {
+    let scrollPositionY = window.scrollY || window.pageYOffset;
+    if (scrollPositionY >= heroHeight) {
+      header.classList.remove("active-state");
+      logo.src = logoBlack;
+    } else {
+      header.classList.add("active-state");
+      logo.src = logoWhite;
+    }
+  }
+})
+
+
+
+
+
+/* ==================== */
+  // PRODUCTS
+/* ==================== */
