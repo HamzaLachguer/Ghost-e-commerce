6706968854e19b0d1e8d87a55c25fb4191775639
hamzalachguer <lachguerhmza@gmail.com>
2025-05-19 13:49:21 +0200

    creating the product grid data

---
+++ products.js
@@ -0,0 +1,199 @@
+const productGrid = [
+  {
+    id: "0000-qwer-00qw",
+    title: "Air Jordan 1 Retro High OG",
+    category: "Men's Shoes",
+    price: 180,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/693f9b4d-95da-4df4-916e-f05c35c850d8/AIR+JORDAN+1+RETRO+HIGH+OG.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/448d4de8-5cb8-406f-8d72-6a4d7092900e/AIR+JORDAN+1+RETRO+HIGH+OG.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/81f83627-7b08-4d31-9890-5681b6f56ae4/AIR+JORDAN+1+RETRO+HIGH+OG.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/8945d8f0-28aa-4844-94fe-7e8fa8898a16/AIR+JORDAN+1+RETRO+HIGH+OG.png"
+    ],
+    details: "You know the colors. You know the history. MJ's college years catapulted him to early stardom, and this reimagined AJ1 High nods back to his North Carolina roots. Distressed leather, classic color blocking and special packaging add a vintage feel—as if you've been saving this pair since 1985.",
+    size: [
+      "7",
+      "8",
+      "9",
+      "10",
+      "11"
+    ]
+  },  {
+    id: "0011-tyui-00er",
+    title: "Nike Air Max Dn8",
+    category: "Men's Shoes",
+    price: 190,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/25f54688-f108-45e9-acf8-4d6ab978fb4d/AIR+MAX+DN8.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/5e7f992b-2ff9-40e3-b007-c9fb1a044346/AIR+MAX+DN8.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/37874b33-f0eb-4b10-95b9-3824970feed8/AIR+MAX+DN8.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/acd3b6fa-6e42-41b9-882e-34ed530c85e4/AIR+MAX+DN8.png"
+    ],
+    details: "More Air, less bulk. The Dn8 takes our Dynamic Air system and condenses it into a sleek, low-profile package. Powered by eight pressurized Air tubes, it gives you a responsive sensation with every step. Enter an unreal experience of movement.",
+    size: [
+      "7",
+      "8",
+      "9",
+      "10",
+      "11"
+    ]
+  },  {
+    id: "0022-opas-00ty",
+    title: "Nike Vomero 18",
+    category: "Men's Road Running Shoes",
+    price: 150,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/e3a103c4-e874-4a24-81d3-62b6f3fa58ca/NIKE+VOMERO+18.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b4347c1c-609b-43a3-bc1a-7fc1dbd7a329/NIKE+VOMERO+18.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/3316ca5c-c029-451d-aa79-c6fe6388fbba/NIKE+VOMERO+18.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/364427c9-6152-4894-b8a2-dcf10117a100/NIKE+VOMERO+18.png"
+    ],
+    details: "Maximum cushioning in the Vomero provides a comfortable ride for everyday runs. Our softest, most cushioned ride has lightweight ZoomX foam stacked on top of responsive ReactX foam in the midsole. Plus, a redesigned traction pattern offers a smooth heel-to-toe transition.",
+    size: [
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11"
+    ]
+  },  {
+    id: "0033-dfgh-00ui",
+    title: "Nike Pegasus Premium",
+    category: "Men's Road Running Shoes",
+    price: 210,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/02d26d72-f11e-4cf7-bb2a-826c13911985/NIKE+PEGASUS+PREMIUM.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/ac3e7107-d6ba-44ab-a870-8e1b05af2a07/NIKE+PEGASUS+PREMIUM.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/697accbe-3975-4fb6-b980-4bba54771531/NIKE+PEGASUS+PREMIUM.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/1a02e294-c511-4850-8ad0-e63ff7112f66/NIKE+PEGASUS+PREMIUM.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/c53b0924-6d0f-4b48-bfa1-9c053db2efb6/NIKE+PEGASUS+PREMIUM.png"
+    ],
+    details: "The Pegasus Premium supercharges responsive cushioning with a triple stack of our most powerful running technologies: ZoomX foam, a sculpted Air Zoom unit and ReactX foam. It’s the most responsive Pegasus ever, providing high energy return unlike any other. With a lighter-than-air upper, it decreases weight and increases breathability so you can fly faster.",
+    size: [
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  },  {
+    id: "0044-jklz-00op",
+    title: "Nike Air Force 1 Low Canvas",
+    category: "Men's Shoes",
+    price: 100,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/6c4b0550-7607-48fa-8ae3-0cee45c05832/AIR+FORCE+1+LOW+CANVAS.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/c767df2b-2a89-4d2e-a20b-a737de9cf65e/AIR+FORCE+1+LOW+CANVAS.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/3724e2e3-e883-47be-9e20-208d578dfebb/AIR+FORCE+1+LOW+CANVAS.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/a434709b-f59e-4daf-9973-435ca2433b38/AIR+FORCE+1+LOW+CANVAS.png"
+    ],
+    details: "Comfortable, durable and timeless—it’s number one for a reason. The classic ‘80s construction pairs with durable canvas for style that tracks whether you’re on court or on the go.",
+    size: [
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11"
+    ]
+  },  {
+    id: "0055-xcvb-00as",
+    title: "Nike Club",
+    category: "Men's Oversized French Terry Pullover Hoodie",
+    price: 99,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/96679ab7-a231-44f3-8879-61dc522b8bc3/M+NK+CLUB+FT+OS+PO+HDY+TRBL.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/7ec8c77d-6971-45f7-9ed2-77ccba376cea/M+NK+CLUB+FT+OS+PO+HDY+TRBL.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/5320c792-afc4-4ca0-9403-51c765f4c556/M+NK+CLUB+FT+OS+PO+HDY+TRBL.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/281f315b-2836-4a9f-ac6e-891b5cb4e664/M+NK+CLUB+FT+OS+PO+HDY+TRBL.png"
+    ],
+    details: "Go big on comfort with this spacious French terry hoodie. Densely knit, with unbrushed loops on the inside, its thick, heavyweight fabric offers an extra-warm feel.",
+    size: [
+      "S",
+      "M",
+      "L",
+      "XL",
+      "XXL"
+    ]
+  },  {
+    id: "0066-NMQW-00df",
+    title: "Nike Club",
+    category: "Men's Fleece Crew",
+    price: 70,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/22b67b84-9937-4bac-880b-709f332c2016/M+NK+CLUB+BB+CREW+NSW+GFX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/ebb66b2f-11ae-4c47-9218-35fd2ed00471/M+NK+CLUB+BB+CREW+NSW+GFX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/6ac847c0-cc50-474e-ab64-f4d2968bad60/M+NK+CLUB+BB+CREW+NSW+GFX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/c2604053-001d-4d3d-be80-d0d2794a52df/M+NK+CLUB+BB+CREW+NSW+GFX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f914cc19-0d29-496c-b5b4-3ead99567aaf/M+NK+CLUB+BB+CREW+NSW+GFX.png"
+    ],
+    details: "Clean meets casual with this classic crew. Smooth on the outside, super soft on the inside, it delivers comfort you can count on.",
+    size: [
+      "S",
+      "M",
+      "L",
+      "XL",
+      "XXL"
+    ]
+  },  {
+    id: "0077-erty-00gh",
+    title: "Nike Tech",
+    category: "Men's Fleece Hoodie",
+    price: 150,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/77833a5b-cda5-4f88-b3b3-f25d2dc15696/M+NK+TCH+FLC+HOODIE+GX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/d2228ee1-0553-4c0e-b71d-8373aa80dcb2/M+NK+TCH+FLC+HOODIE+GX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/a4f329e6-0f92-4a12-8dd7-6214e5208b3b/M+NK+TCH+FLC+HOODIE+GX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/7835f16a-674e-4575-b4a5-995897d6e23f/M+NK+TCH+FLC+HOODIE+GX.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/acbb2aba-42d3-4a4c-83e5-caf411e15e8e/M+NK+TCH+FLC+HOODIE+GX.png"
+    ],
+    details: "We took our iconic smooth-on-both-sides fleece and added extra weight for more structure and warmth. We paired that with an extra roomy, oversized fit and premium graphic details to create a new take on the classic hoodie.",
+    size: [
+      "S",
+      "M",
+      "L",
+      "XL",
+      "XXL",
+      "XXXL"
+    ]
+  },  {
+    id: "0088-xcvb-00jk",
+    title: "Nike Air Max 270",
+    category: "Women's Shoes",
+    price: 160,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/7f20343a-5099-4787-a8ce-52587e878b0e/W+AIR+MAX+270.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/15c7ba73-14a3-4de7-b3f0-a870e91d15ea/W+AIR+MAX+270.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/61d74566-4469-45a0-8ccd-51937973af86/W+AIR+MAX+270.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/cbc85968-c252-43a5-8b43-9b7a3e1350ff/W+AIR+MAX+270.png"
+    ],
+    details: "Nike's first lifestyle Air Max brings you style, comfort and big attitude in the Nike Air Max 270. The design draws inspiration from Air Max icons, showcasing Nike's greatest innovation with its large window and fresh array of colors.",
+    size: [
+      "7",
+      "8",
+      "9",
+      "10",
+      "11"
+    ]
+  },  {
+    id: "0099-nmqw-00lz",
+    title: "Jordan Brooklyn Fleece",
+    category: "Women's Crew-Neck Sweatshirt",
+    price: 68,
+    imgs: [
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/3585e773-2fec-4614-9159-1c282ef7dfd8/W+J+BRKLN+FLC+CREW+GFX+J84.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/8c6031f4-287f-46fb-89c2-8fe321188c33/W+J+BRKLN+FLC+CREW+GFX+J84.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/9468f89a-8620-4f04-907e-34547d88ef94/W+J+BRKLN+FLC+CREW+GFX+J84.png",
+      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/45636067-f3ab-4453-b6a0-897f0c6dc08f/W+J+BRKLN+FLC+CREW+GFX+J84.png"
+    ],
+    details: "We reached into our archives for this one. Vintage Jordan graphics give this roomy layer a retro vibe. Midweight French terry fabric keeps it lightweight but warm. Some things never go out of style.",
+    size: [
+      "S",
+      "M",
+      "L",
+      "XL",
+      "XXL"
+    ]
+  },
+]
